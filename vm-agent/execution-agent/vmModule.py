e2384596717dd69cb3c04f90036c7e2774ec9abc
Mohamed Elsakhawy <mfouadga@uwo.ca>
2022-05-06 17:27:57 +0000

    Docker SDK integrated, Dockerfile updated to include ffmpeg install for all containers --Not Optimal. vmModule.py now checks for images locally and containerizes when needed. Remaning is the memsize for the running containers

--- vm-agent/execution-agent/vmModule.py
+++ vm-agent/execution-agent/vmModule.py
@@ -13,8 +13,19 @@ import Text2SpeechWorkflow
 import ImageProcessing
 import subprocess
 from google.protobuf.json_format import MessageToJson
+import docker
+from google.cloud import functions_v1
+import requests
+import wget
+import string
+from zipfile import ZipFile
+import subprocess
+import sys
+
+
 project_id = "ubc-serverless-ghazal"
 subscription_id = "vmSubscriber1"
+
 #publish_topic_id = "vm-subscribe"
 #timeout = 22.0
 
@@ -24,6 +35,52 @@ subscriber = pubsub_v1.SubscriberClient()
 subscription_path = subscriber.subscription_path(project_id, subscription_id)
 DSclient = datastore.Client()
 publisher = pubsub_v1.PublisherClient()
+client = docker.from_env()
+
+def containerize(functionname):
+    # Create a client
+    client = functions_v1.CloudFunctionsServiceClient()
+
+    # Initialize request arguments
+    request = functions_v1.GenerateDownloadUrlRequest(name="projects/ubc-serverless-ghazal/locations/northamerica-northeast1/functions/"+functionname,
+    )
+
+    # Make the request
+    response = client.generate_download_url(request=request)
+    downloadlink = str(response).split(' ')[1].split('"')[1]
+    # Download the function
+    print ("\nDownloading the function")
+    wget.download(downloadlink,functionname+'.zip')
+    request = functions_v1.GetFunctionRequest(
+       name="projects/ubc-serverless-ghazal/locations/northamerica-northeast1/functions/"+functionname,
+    )
+
+    # Make the request
+    response = client.get_function(request=request)
+    entrypoint = response.entry_point
+
+    # Unzip the function
+    print ("\nUnzipping the function")
+    with ZipFile(functionname+'.zip', 'r') as zipObj:
+       zipObj.extractall(functionname)
+    with open("/tmp/output.log", "a") as output:
+       print ("\nCreating the Docker container \n")
+       # Copy the Docker file to the unzipped folder
+       subprocess.call("cp Dockerfile "+functionname, shell=True, stdout=output, stderr=output)
+       subprocess.call("cp init.sh "+functionname, shell=True, stdout=output, stderr=output)
+       file_object = open(functionname+'/main.py', 'a')
+       file_object.write('import sys\n')
+       file_object.write('def main():\n')
+       file_object.write('    '+entrypoint+'(json.loads(sys.argv[1]),"dummy")\n')
+       file_object.write("if __name__ == '__main__':\n")
+       file_object.write('    main()\n')
+       file_object.close()
+       subprocess.call("cp Text2Speech/"+functionname + "/requirements.txt "+ functionname+"/requirements.txt" , shell=True, stdout=output, stderr=output)
+       subprocess.call("cp ubc-serverless-ghazal-9bede7ba1a47.json "+functionname + "/ "  , shell=True, stdout=output, stderr=output)
+       subprocess.call("sed -i 's/json.loads(base64.b64decode//g' "+functionname + "/main.py " , shell=True, stdout=output, stderr=output)
+       subprocess.call('sed -i "s/.decode(\'utf-8\'))//g" ' + functionname + "/main.py " , shell=True, stdout=output, stderr=output)
+       # Create the image from the Dockerfile also copy the function's code
+       subprocess.call("cd "+functionname+"; docker build . < Dockerfile --tag name:"+functionname, shell=True, stdout=output, stderr=output)
 
 def callback(message: pubsub_v1.subscriber.message.Message) -> None:
     global writtenData
@@ -35,6 +92,13 @@ def callback(message: pubsub_v1.subscriber.message.Message) -> None:
     routingData = message.attributes.get("routing")
     reqID = message.attributes.get("reqID")
     invokedFun = message.attributes.get("invokedFunction")
+    notfound=1
+    for image in client.images.list():
+        if invokedFun in str(image.tags):
+            print ("Image found")
+            notfound=0
+    if notfound == 1:
+        containerize(invokedFun)
     writtenData[msgID] = {}
     writtenData[msgID]["receivedDate"] = str(receivedDateObj)
     if message.attributes:
@@ -51,7 +115,13 @@ def callback(message: pubsub_v1.subscriber.message.Message) -> None:
             }
     print (str(jsonfile).replace('\'','"'))
     with open("/tmp/output.log", "a") as output:
-        subprocess.call("docker run name:"+ invokedFun+" '"+  str(jsonfile).replace('\'','"') + "' " + reqID , shell=True, stdout=output, stderr=output)
+        before  = datetime.datetime.now()
+        #subprocess.call("docker run name:"+ invokedFun+" '"+  str(jsonfile).replace('\'','"') + "' " + reqID , shell=True, stdout=output, stderr=output)
+        #subprocess.call("docker run name:"+ invokedFun+" "+  str(jsonfile).replace('\'','"') + "' " + reqID , shell=True, stdout=output, stderr=output)
+        client.containers.run("name:"+ invokedFun,command="python3 /app/main.py '"+  str(jsonfile).replace('\'','"') + "' " + reqID )
+        after  = datetime.datetime.now()
+        delta =  after - before
+        print ("Docker Execution Durations for "+invokedFun + " is " +str(delta.microseconds)+" microseconds")
 def func1(msg):
     decodedMessage = json.loads(msg.decode("utf-8"))
     msg = decodedMessage["data"]["message"]
