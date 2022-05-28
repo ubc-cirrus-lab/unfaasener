23db916e4d3a101f11a050d59a6cb6456e6ab648
Mohamed Elsakhawy <mfouadga@uwo.ca>
2022-05-28 19:29:01 +0000

    Added multi-vm-agent executions#   python3 execution-agent.py  {NUMBER_OF_AGENTS}

---
+++ vm-agent/execution-agent/execution-agent.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import sys
+
+
+def main():
+    for i in range(0,int(sys.argv[1])):
+      with open("/tmp/output.log", "a") as output:
+         subprocess.Popen("python3 vmModule.py" , shell=True, stdout=output, stderr=output)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
