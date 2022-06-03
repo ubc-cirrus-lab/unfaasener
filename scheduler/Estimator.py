fed169815fa57dc456a8944ebdb29317c5e46468
Ghazal Sadeghian <sadeghian.ghazal77@gmail.com>
2022-06-02 19:55:13 -0700

    Removed the unnecessary import of seaborn library

--- scheduler/Estimator.py
+++ scheduler/Estimator.py
@@ -8,11 +8,8 @@ import time
 import os
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import math
-import networkx as nx
 from monitoring import monitoring
-from criticalpath import Node
 from pathlib import Path
 import rankerConfig
 import statistics
