351ef65ed22fddb1529d513b22753fbd7970e780
GhazalSdn <sadeghian.ghazal77@gmail.com>
2022-08-23 20:13:39 +0000

    cleaup Module for keep-alived for containers

---
+++ vm-agent/execution-agent/cleanupModule.py
@@ -0,0 +1,31 @@
+import docker
+import sys
+import time
+from datetime import datetime, timedelta
+
+
+def main():
+#    run_container(sys.argv[1])
+
+    client_api = docker.APIClient(base_url='unix://var/run/docker.sock')
+    info = client_api.df()
+    for container in info['Containers']:
+      if 'Created' in container:
+        print(container['Created'])
+        created_at = datetime.fromtimestamp((container['Created']))
+        print (created_at)
+        if (created_at + timedelta(seconds=int(sys.argv[2])) < datetime.now()):
+            print ("Container too old... Stopping It")
+            client_api.stop(container)
+
+
+
+
+
+if __name__ == '__main__':
+    while True:
+      time.sleep(int(sys.argv[1]))
+      main()
+
+
+
