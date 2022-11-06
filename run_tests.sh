bdd6ddc5fc5a9dde0a06b6420da3e79fcb9efa85
Mohammad Shahrad <mshahrad@load-generator.arbutus>
2022-11-06 03:17:32 +0000

    added basic tests for the predictor + polished the predictor code + throwing exception when buffer size is zero

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+solver tests
 cp ./tests/solver/rps_cost_test.py ./scheduler
 cp ./tests/solver/rps_latency_test.py ./scheduler
 cd ./scheduler
-
 if command -v python &> /dev/null
 then
     python rps_cost_test.py
@@ -13,6 +13,13 @@ then
     python3 rps_cost_test.py
     python3 rps_latency_test.py 
 fi
-
 rm rps_cost_test.py
 rm rps_latency_test.py
+cd ../
+
+# host agent tests
+cp ./tests/host_agents/predictor_test.cpp vm-agent/monitoring-agent/
+cd vm-agent/monitoring-agent
+g++ -std=c++14 -I . predictor_test.cpp -o predictor_test
+./predictor_test
+rm ./predictor_test*
--- vm-agent/monitoring-agent/predictor.h
+++ vm-agent/monitoring-agent/predictor.h
@@ -15,69 +15,43 @@ class predictor
     double sum,standardDeviation = 0.0,mean = 0.0;
     double max,alpha,margin,prediction = 0.0;
     int size;
+    
 public:
     predictor(ring* buffer)
     {
         utilization_records = buffer;
-	sum=0;
+	    sum = 0;
     }
-    double compute_predicton_ServerMore() 
+    
+    auto compute_predicton_ExponentialMovingAverage(double x, int type, int initialFlag)
     {
-     mean = 0;
-     sum = 0;
-     standardDeviation = 0;
-     size=utilization_records->size();
-     int i = 0;
-     double records[size];
-     while (utilization_records->size() > 0)
-	{
-	records[i] = utilization_records->pop();	
-	i++;
-	}
-    //Compute Mean for the records array
-    for(i = 0; i < size; ++i) {
-	sum += records[i];
-    }
-    mean = sum/size;
-    // Compute Std Dev.
-    for(i = 0; i < size; ++i) {
-    standardDeviation += pow(records[i] - mean, 2);
-    standardDeviation = standardDeviation/size;
-    standardDeviation = sqrt(standardDeviation);
-    }
-    std::cout << "Time window's Mean is  "<< mean <<" and StdDev is "<< standardDeviation << std::endl;
-    return  standardDeviation + mean;
-    }
-    auto compute_predicton_ExponentialMovingAverage(double x,int type, int initialFlag)
-    {
-     size_t violation = 0;
-     alpha = 0.85;
-     margin = 0.2;
-     max = 0;
-     size=utilization_records->size();
-     int i = 0;
-     double records[size];
-     while (utilization_records->size() > 0)
+        size_t violation = 0;
+        alpha = 0.85;
+        margin = 0.2;
+        max = 0;
+        size = utilization_records->size();
+        if (size == 0)
+            throw std::logic_error( "Exception: record buffer has size 0!" );
+        int i = 0;
+        double records[size];
+        while (i < size)
         {
-        records[i] = utilization_records->pop();
-        i++;
+            records[i] = utilization_records->pop();
+            i++;
         }
-    //Compute Max for the records array
-    for(i = 0; i < size; ++i) {
-        if (records[i] > max)
-	{
-		max = records[i];
-	}
-
+        // calculate the max for the records array
+        for(i = 0; i < size; ++i) {
+            if (records[i] > max)
+            {
+                max = records[i];
+            }
+        }
+        prediction = (alpha *  max  + (1-alpha)*x );
+        if ( prediction * (1+margin) > 100)
+        prediction = 100 ;
+        if ( (int(max) - int(x) > 100*margin ) || (int(x) - int(max) > 100*margin ) || (prediction > x + 100*margin)|| (prediction + 100 *margin < x ) || (initialFlag == 1))
+            violation = 1;
+        struct result {double prediction; size_t violation;};
+        return result {prediction, violation};
     }
-
-    prediction = (alpha *  max  + (1-alpha)*x );
-    if ( prediction * (1+margin) > 100)
-	   prediction = 100 ;
-    if ( (int(max) - int(x) > 100*margin ) || (int(x) - int(max) > 100*margin ) || (prediction > x + 100*margin)|| (prediction + 100 *margin < x ) || (initialFlag == 1))
-        violation = 1;
-    struct result {double prediction; size_t violation;};
-    return result {prediction, violation};
-    }
- 
 };
