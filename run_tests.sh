f5950542bf553fcb6ad492fbe14aeb33d85af587
Ghazal Sadeghian <45939148+GhazalSdn@users.noreply.github.com>
2022-12-01 18:15:30 -0800

    Data Cleanup (#73)
    * cleaned directory
    
    * Added scripts for adding test case data and creating workflow directories
    
    * Modified the test
    
    * Added test case data
    
    * Cleaned log parser data directory + Added the data collection in the test script + removed intermediate plots and move to to results repo
    
    * Removed plots
    
    * Delete scheduler/plots directory
    
    * Fixed the problem in unit tests caused by removing directories + Added collected logs for the test cases

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # solver tests
+# get data
+cp -a ./tests/solver/data/. ./scheduler/data/
+cp -a ./tests/logCollector/. ./log_parser/get_workflow_logs/data/
 cp ./tests/solver/rps_cost_test.py ./scheduler
 cp ./tests/solver/rps_latency_test.py ./scheduler
 cd ./scheduler
@@ -15,8 +18,19 @@ then
 fi
 rm rps_cost_test.py
 rm rps_latency_test.py
-cd ../
-
+cd ./data
+arr=("TestCase10Workflow" "TestCase11Workflow" "TestCase2Workflow" "TestCase3Workflow" "TestCase4Workflow" "TestCaseWorkflow")
+for item in "${arr[@]}"
+do
+    rm -rf "$item"
+done
+cd ../../
+cd ./log_parser/get_workflow_logs/data
+for item in "${arr[@]}"
+do
+    rm -rf "$item"
+done
+cd ../../../
 # host agent tests
 cp ./tests/host_agents/predictor_test.cpp vm-agent/monitoring-agent/
 cd vm-agent/monitoring-agent
--- setup.sh
+++ setup.sh
@@ -6,3 +6,15 @@ sudo apt install -y docker.io
 sudo apt install -y libcurl*
 sudo apt-get install -y libpstreams-dev
 sudo  usermod -a -G docker  $USER
+arr=("ChatBotWorkflow" "DNAVisualizationWorkflow" "ImageProcessingWorkflow" "RegressionTuningWorkflow" "Text2SpeechCensoringWorkflow" "VideoAnalyticsWorkflow")
+cd ./scheduler/data
+for dirname in "${arr[@]}"
+do
+    mkdir "$dirname"
+done 
+cd ../../
+cd ./log_parser/get_workflow_logs/data
+for dirname in "${arr[@]}"
+do
+    mkdir "$dirname"
+done 
