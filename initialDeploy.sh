0cbce148a55fa5e5b16f97add26fdaca85cecde6
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2023-05-29 22:22:43 -0700

    using relative paths with respect to home dir of project for the initdeply script

--- initialDeploy.sh
+++ initialDeploy.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
+
 workflow="$1"
 hostcount="$2"
 solvingMode="$3"
 mode="resolve"
 
+# please run this script from the home directory of the unfaasener project
+
 # making sure that by default are functions run on serverless
-# python3 ~/unfaasener/scheduler/resetRoutingDecisions.py $workflow $hostcount
+# python3 scheduler/resetRoutingDecisions.py $workflow $hostcount
 docker container stop $(docker container ls -aq)
 # clean all privious metadata, logs, and caches for that workflow
-python3 ~/unfaasener/scheduler/resetLastDecisions.py $workflow $hostcount $solvingMode
+python3 scheduler/resetLastDecisions.py $workflow $hostcount $solvingMode
 
 # clean the host execution agent queue
-python3 ~/unfaasener/host-agents/execution-agent/cleanup-queue.py vmSubscriber1 vm0
+python3 host-agents/execution-agent/cleanup-queue.py vmSubscriber1 vm0
 
 # get the most recent serverless logs for the specified workflow
-python3 ~/unfaasener/log-parser/get-workflow-logs/getWorkflowLogs.py $workflow 1 
+python3 log-parser/get-workflow-logs/getWorkflowLogs.py $workflow 1 
 
 # running the monitoring scripts and the predictor agent in the background
-cd ~/unfaasener/host-agents/monitoring-agent
+cd host-agents/monitoring-agent
 ./monitoringAgent &
 pidFour=$!
 sleep 10
+
 # running the vm execution agent in the background
-python3 ~/unfaasener/host-agents/execution-agent/vmModule.py vmSubscriber1 vm0 &
+cd ../execution-agent
+python3 vmModule.py vmSubscriber1 vm0 &
 pidOne=$!
 
 # periodically (every 10m) collecting the execution logs of the workflow in the background
-python3 ~/unfaasener/log-parser/get-workflow-logs/getWorkflowLogs.py $workflow 0 &
+cd ../..
+python3 log-parser/get-workflow-logs/getWorkflowLogs.py $workflow 0 &
 pidTwo=$!
 
 # periodically performing garbage collection (every 1h) on the datastore(unused merging points data) and the dataframe
-python3 ~/unfaasener/scheduler/garbageCollector.py &
+python3 scheduler/garbageCollector.py &
 pidThree=$!
 
 
 
 # terminating background processes upon termination of this script
-trap "kill ${pidOne} ${pidTwo} ${pidThree} ${pidFour}; python3 ~/unfaasener/log-parser/get-workflow-logs/getWorkflowLogs.py ${workflow} 1; python3 ~/unfaasener/setup-tests/combineDataframes.py; exit 1" INT SIGINT SIGTERM EXIT
+trap "kill ${pidOne} ${pidTwo} ${pidThree} ${pidFour}; python3 log-parser/get-workflow-logs/getWorkflowLogs.py ${workflow} 1; python3 setup-tests/combineDataframes.py; exit 1" INT SIGINT SIGTERM EXIT
 wait
 
 
