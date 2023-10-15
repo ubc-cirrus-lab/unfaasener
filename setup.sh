340c31074d2f177bb6de2ce89a22ed9a28e8f1b8
engshahrad <engshahrad@users.noreply.github.com>
2023-10-15 06:46:18 +0000

    fixed issues with the setup script: 1) install Julia 1.9.3 if not installed and 2) check if directories exist before creating them

--- setup.sh
+++ setup.sh
@@ -5,9 +5,42 @@ if command -v julia &> /dev/null
 then
     julia setupJulia.jl
 else
-    echo "Error: Please install Julia and then rerun this script."
-    exit 1
-fi 
+    echo "Error: Julia is not installed on your system."
+    echo "Do you want to install Julia? (y/n)"
+    read installJulia
+    if [ $installJulia == "y" ]
+    then
+        if [ "$(uname)" == "Linux" ] && [ "$(uname -m)" == "x86_64" ]
+        then
+            echo "Downloading Julia..."
+            wget https://julialang-s3.julialang.org/bin/linux/x64/1.9/julia-1.9.3-linux-x86_64.tar.gz
+            echo "Extracting Julia..."
+            tar -xvzf julia-1.9.3-linux-x86_64.tar.gz
+            echo "Installing Julia..."
+            sudo cp -r julia-1.9.3 /opt/
+            sudo ln -s /opt/julia-1.9.3/bin/julia /usr/local/bin/julia
+            echo "Julia has been installed."
+        elif [ "$(uname)" == "Linux" ] && [ "$(uname -m)" == "aarch64" ]
+        then
+            echo "Downloading Julia..."
+            wget https://julialang-s3.julialang.org/bin/linux/aarch64/1.9/julia-1.9.3-linux-aarch64.tar.gz
+            echo "Extracting Julia..."
+            tar -xvzf julia-1.9.3-linux-aarch64.tar.gz
+            echo "Installing Julia..."
+            sudo cp -r julia-1.9.3 /opt/
+            sudo ln -s /opt/julia-1.9.3/bin/julia /usr/local/bin/julia
+            echo "Julia has been installed."
+        else
+            echo "Please download Julia from https://julialang.org/downloads/, install it, and then re-run this script."
+            exit 1
+        fi
+        julia setupJulia.jl
+    else
+        echo "Please install Julia and re-run this script."
+        exit 1
+    fi
+fi
+
 pip3 install -r ./requirements.txt
 sudo apt install -y docker.io
 sudo apt-get install -y libpstreams-dev
@@ -17,32 +50,62 @@ arr=("DNAVisualizationWorkflow" "ImageProcessingWorkflow" "RegressionTuningWorkf
 cd ./scheduler/data
 for dirname in "${arr[@]}"
 do
-    mkdir "$dirname"
+    if [ -d "$dirname" ]
+    then
+        echo "Directory $dirname exists."
+    else
+        mkdir "$dirname"
+    fi
 done 
 cd ../
-mkdir logs
+if [ -d "logs" ]
+then
+    echo "Directory logs exists."
+else
+    mkdir logs
+fi
 cd ../
 cd ./log-parser/get-workflow-logs/data
 for dirname in "${arr[@]}"
 do
-    mkdir "$dirname"
+    if [ -d "$dirname" ]
+    then
+        echo "Directory $dirname exists."
+    else
+        mkdir "$dirname"
+    fi
 done 
 cd ../
-mkdir logs
+if [ -d "logs" ]
+then
+    echo "Directory logs exists."
+else
+    mkdir logs
+fi
 cd ../../
 cd ./host-agents/execution-agent
-mkdir logs
-mkdir data
+if [ -d "logs" ]
+then
+    echo "Directory logs exists."
+else
+    mkdir logs
+fi
+if [ -d "data" ]
+then
+    echo "Directory logs exists."
+else
+    mkdir data
+fi
 cd ../../
 if [ $leaderFailure -eq 1 ]
 then
-cd ./log-parser/get-workflow-logs
-if command -v python &> /dev/null
-then
-    python getNewDatastoreLogs.py
-elif command -v python3 &> /dev/null
-then
-    python3 getNewDatastoreLogs.py
-fi 
+    cd ./log-parser/get-workflow-logs
+    if command -v python &> /dev/null
+    then
+        python getNewDatastoreLogs.py
+    elif command -v python3 &> /dev/null
+    then
+        python3 getNewDatastoreLogs.py
+    fi
 fi
 echo "Please exit your current session and relogin"
